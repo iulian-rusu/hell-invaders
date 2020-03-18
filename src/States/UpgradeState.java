aab5fa7511ceb779de001d7491778cf0fde78a7a
Iulian <iulian.rusu999@gmail.com>
2020-03-19 01:07:25 +0200

    Initial commit.

---
+++ src/States/UpgradeState.java
@@ -0,0 +1,34 @@
+package States;
+
+import Game.GameWindow.GameWindow;
+
+import java.awt.event.MouseEvent;
+
+public class UpgradeState extends State {
+
+    @Override
+    public void Init() {
+
+    }
+
+    @Override
+    public void Update() {
+        super.Update();
+    }
+
+    @Override
+    public void Draw(GameWindow wnd) {
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent mouseEvent) {
+
+    }
+
+}
